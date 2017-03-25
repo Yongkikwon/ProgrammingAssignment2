47f031be9e23e541243222fa82f688acc6d2c094
Yongki Kwon <outlieer@gmail.com>
2017-03-25 17:24:56 +0900

    R Programming Assi 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        rmtx <- NULL
+        
+        setmtx <- function(y) {   # set the value of the matris
+                x <<- y
+                rmtx <<- NULL
+        }
+        getmtx <- function() x    #get the value of the matrix 
+        setinver <- function(solve) rmtx <<- solve #set the value of the inverse matrix  
+        getinver <- function() rmtx  #get the value of the inverse matrix  
+        
+        list(getmtx = getmtx,
+             setinver = setinver,
+             getinver = getinver)
 }
 
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        rmtx <- x$getinver() # set inverse values of the 'x'
+        # if store inverse vlaues are exist return values in the cache. 
+        if(!is.null(rmtx)){
+                message("getting cached data")
+                return(rmtx)
+        }
+        # if store inverse vlaues are not exist
+        mtx <- x$getmtx()    # store matrix value of the 'x' into 'mtx'
+        rmtx <- solve(mtx)   # store inverse matrix value of the 'x' into 'rmtx'
+        x$setinver(rmtx)     # store inverse value into cache
+        rmtx
 }
